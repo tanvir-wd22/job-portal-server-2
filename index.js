72a0fca6387bfd0f7b4de8c97389a637fa88286b
tanvir-wd22 <tanvirwd22@gmail.com>
2026-03-25 07:47:30 +0600

    alhamdulliah jobs read operation done

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 // database
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.oko4eb5.mongodb.net/?appName=Cluster1`;
-console.log(uri);
+// console.log(uri);
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -29,13 +29,18 @@ async function run() {
     // Connect the client to the server
     await client.connect();
     console.log('db connected successfully');
+    // Get the database and collection on which to run the operation
+    const database = client.db('jobsPortalDB2');
+    const jobsCollection = database.collection('jobsColl');
     //=====================================================
 
+    // jobs read opreation
+    app.get('/jobs', async (req, res) => {
+      const cursor = jobsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-
-
-
-    
     //=====================================================
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 });
